c951f933fbac2f54939de33e1e7c8ffd7f852f32
Md Arafat Akhter <arafatakhterforhad@gmail.com>
2026-01-19 13:15:11 +0600

    notification banner update

--- components/dashboard/qr-codes/ReviewBanner.tsx
+++ components/dashboard/qr-codes/ReviewBanner.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 
 export default function ReviewBanner() {
   return (
-    <div className="flex items-center flex-col tablet:flex-row self-stretch tablet:gap-6 gap-[10px] p-4 rounded-[var(--Corner-Radius-10)] bg-[var(--Purple)] desktopDashboard:my-1 my-0">
+    <div className="flex items-center flex-col tablet:flex-row desktopDashboard:flex-row self-stretch tablet:gap-6 desktopDashboard:gap-6 gap-[10px] p-4 rounded-[var(--Corner-Radius-10)] bg-[var(--Purple)] desktopDashboard:my-1 my-0">
       <div className="flex items-center gap-2 flex-1">
         <FaceSmile className="w-6 h-6 shrink-0" />
         <p className="text-white text-[16px] leading-[24px]">
@@ -12,7 +12,7 @@ export default function ReviewBanner() {
         </p>
       </div>
 
-      <div className="flex items-center gap-2 w-full tablet:w-auto">
+      <div className="flex items-center gap-2 w-full tablet:w-auto desktopDashboard:w-auto">
         <Button className="bg-white text-[var(--Dark-gray)] rounded-[var(--Corner-Radius-10)] py-2 px-4 text-[14px] leading-[22px] h-10 flex-1">
           Yes
         </Button>
--- components/dashboard/qr-codes/SubscribeBanner.tsx
+++ components/dashboard/qr-codes/SubscribeBanner.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 
 export default function SubscribeBanner() {
   return (
-    <div className="flex items-center flex-col tablet:flex-row self-stretch tablet:gap-6 gap-[10px] p-4 rounded-[var(--Corner-Radius-10)] bg-[#E7E8EE] desktopDashboard:my-1 my-0">
+    <div className="flex items-center flex-col tablet:flex-row desktopDashboard:flex-row self-stretch tablet:gap-6 desktopDashboard:gap-6 gap-[10px] p-4 rounded-[var(--Corner-Radius-10)] bg-[#E7E8EE] desktopDashboard:my-1 my-0">
       <div className="flex items-center gap-2 flex-1">
         <AlertCircle className="w-6 h-6 shrink-0" />
         <p className="text-[var(--Dark-gray)] text-[16px] leading-[24px]">
@@ -12,7 +12,7 @@ export default function SubscribeBanner() {
         </p>
       </div>
 
-      <Button className="bg-[var(--Blue)] text-white rounded-[var(--Corner-Radius-10)] py-2 px-4 text-[14px] leading-[22px] h-10 hover:bg-[var(--Blue-hover)] transition-all duration-300 ease-linear w-full tablet:w-auto">
+      <Button className="bg-[var(--Blue)] text-white rounded-[var(--Corner-Radius-10)] py-2 px-4 text-[14px] leading-[22px] h-10 hover:bg-[var(--Blue-hover)] transition-all duration-300 ease-linear w-full tablet:w-auto desktopDashboard:w-auto">
         Subscribe
       </Button>
     </div>
