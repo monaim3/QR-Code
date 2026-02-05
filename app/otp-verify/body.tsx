1dee41353a3008bacce750784ed5d99a5da2c8c6
mbshuvo325 <mbshuvo2255@gmail.com>
2026-02-06 01:25:13 +0600

    update otp

--- app/otp-verify/body.tsx
+++ app/otp-verify/body.tsx
@@ -65,21 +65,21 @@ export default function TwoFactorAuthPage() {
 
   return (
    <Container>
-    <div className="min-h-screen flex items-center justify-center">
-      <div className="w-full max-w-md">
+    <div className="min-h-screen flex items-start justify-center">
+      <div>
           {/* Card with shadow, rounded corners, padding, but no white background */}
-          <div className="bg-white rounded-[12px] shadow-card flex flex-col items-center p-[24px] desktop:p-[32px]">
+          <div className="w-full max-w-[400px] bg-white rounded-[12px] shadow-card flex flex-col items-center p-[24px] desktop:p-[32px] mt-[136px] desktop:mt-[120px]">
             
             {/* Title */}
             <h1 className="text-[20px] desktop:text-[24px] leading-[28px] desktop:leading-[32px] font-bold text-gray-900">Welcome back!</h1>
 
             {/* Subtitle */}
-            <p className="pt-[8px] text-[16px] leading-[24px] fonr-regular text-gray-600 text-center">
+            <p className="pt-[8px] text-[16px] leading-[24px] fonr-regular text-[var(--Dark-gray)] text-center">
               Enter the Code from Your Two-Factor Authentication App.
             </p>
 
             {/* Code Input */}
-            <div className="pt-[24px] flex gap-[16px] md:gap-3 w-full justify-center">
+            <div className="pt-[24px] flex gap-[8px] desktop:gap-[16px] w-full justify-center">
             {code.map((digit, index) => (
               <input
                 key={index}
@@ -93,7 +93,7 @@ export default function TwoFactorAuthPage() {
                 onChange={(e) => handleChange(index, e.target.value)}
                 onKeyDown={(e) => handleKeyDown(index, e)}
                 onPaste={handlePaste}
-                className="w-[40px] h-[48px] md:w-12 md:h-14 text-center text-lg md:text-xl font-semibold border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:border-transparent transition-all"
+                className="w-[40px] h-[48px] text-center text-[18px] font-semibold border border-[var(--Boarder-Grey)] rounded-[6px] focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:border-transparent transition-all"
                 aria-label={`Digit ${index + 1}`}
               />
             ))}
@@ -101,7 +101,7 @@ export default function TwoFactorAuthPage() {
             {/* Login Button */}
              <button
               onClick={()=> setIsModalOpen(true)}
-              className="w-[318px] desktop:w-full h-[48px] bg-[#01A56D] hover:bg-emerald-700 text-white text-[18px] font-medium leading-[16px] rounded-[10px] transition-colors duration-200 mt-[24px]"
+              className="w-full desktop:w-full h-[48px] bg-[var(--Blue)] hover:bg-[var(--Blue-hover)] text-white text-[18px] font-medium leading-[16px] rounded-[10px] transition-colors duration-300 mt-[24px]"
             >
               Login
             </button>
@@ -112,20 +112,18 @@ export default function TwoFactorAuthPage() {
            />
 
             {/* Help Text */}
-            <p className="pt-[14px] text-[14px] leading-[22px] font-body_text text-gray-500 text-center">
-              If you've lost your 2FA credentials, please reach out 
-            </p>
-
-              <div className="w-full desktop:w-auto text-[14px] leading-[22px] font-normal text-[#3F3E3E] font-body_text flex flex-wrap gap-x-[4px] text-center desktop:text-start justify-center desktop:justify-start">
-              <span>to our</span>
-              <Link href="#" className="text-[#01A56D]">
+            <p className="pt-[24px] text-[14px] leading-[22px] font-body_text text-[var(--Dark-gray)] text-center">
+              If you've lost your 2FA credentials, please reach out to our
+              <span className='px-[4px]'>
+                 <Link href="#" className="text-[var(--Blue)]">
                 Support
               </Link>
-              <span>for Assistance</span>
-              </div>
+              </span>
+              for Assistance
+            </p>
 
             {/* Back Button */}
-            <div className="pt-[32px]">
+            <div className="pt-[24px]">
              <BackButtonWithText title='Back'/>
             </div>
 
