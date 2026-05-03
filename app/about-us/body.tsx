d7a6f02be556c2cb3d9262dc59712e2ece923637
monaim3 <monaimmukul75@gmail.com>
2026-05-03 23:07:27 +0600

    fix about page issue

--- app/about-us/body.tsx
+++ app/about-us/body.tsx
@@ -2,11 +2,6 @@ import Container from "@/components/common/parent-container";
 import Link from "next/link";
 import Image from "next/image";
 import { ChevronRight } from "lucide-react";
-import Wifi from "../../components/icons/wifi";
-import LinkIcon from "../../components/icons/link";
-import VideoCamera from "../../components/icons/video-camera";
-import ImageIcon from "../../components/icons/image-icon";
-import RestaurantIcon from "../../components/icons/restaurant";
 import AboutUsLeftElement from "../../components/about-us/left-elements";
 import OurMission from "../../components/about-us/our-mission";
 import CustomerReview from "../../components/home/customer_review";
@@ -35,123 +30,49 @@ export default function AboutUsBody() {
       <section className="bg-[var(--Generator-Background)] overflow-hidden">
         <Container className="w-full max-w-full">
           {/* Main content */}
-          <div className="flex flex-col w-full h-full pt-[29px] desktop:pt-[120px]">
+          <div className="flex flex-col w-full py-[29px] desktop:py-[120px]">
             <div className="flex flex-col items-center desktop:flex-row w-full min-w-full h-auto desktop:h-[480px]">
               {/* Left half - desktop only */}
               <div className="hidden desktop:block relative z-10">
                 <AboutUsLeftElement />
               </div>
 
-              {/* Right half */}
-              <div className="order-1 desktop:order-2 desktop:flex-1 h-full flex items-center justify-center min-w-0">
+              {/* Right half - desktop */}
+              <div className="hidden desktop:flex order-1 desktop:order-2 desktop:flex-1 h-full items-center justify-center min-w-0">
                 <Image
                   src="/images/generator_img/aboutqr.svg"
                   alt="QR Scan"
                   width={592}
                   height={480}
-                  className="w-full max-w-[592px] h-auto drop-shadow-[0_1.385px_22.154px_rgba(63,72,103,0.08)]"
+                  className="w-[592px] h-[480px] drop-shadow-[0_1.385px_22.154px_rgba(63,72,103,0.08)]"
                 />
+              </div>
+
+              {/* Right half - mobile */}
+              <div className="flex desktop:hidden order-1 -mx-5 w-[calc(100%+2.5rem)] justify-center items-center">
+                <div className="relative w-full">
+                  <Image
+                    src="/images/generator_img/gettoknow.svg"
+                    alt="QR Background"
+                    width={350}
+                    height={207}
+                    className="w-full h-auto"
+                  />
+                  <div className="absolute inset-0 flex items-center justify-center">
+                    <Image
+                      src="/images/generator_img/gettoknowqr.svg"
+                      alt="QR Code"
+                      width={155}
+                      height={155}
+                      className="w-[95%] h-auto"
+                    />
+                  </div>
+                </div>
               </div>
 
               {/* Mobile text - hidden on desktop */}
               <div className="block desktop:hidden order-2 w-full mt-6">
                 <AboutUsLeftElement />
-              </div>
-            </div>
-            <div className="flex pt-[120px] pb-[59px] items-center gap-6">
-              <p className="hidden desktop:block text-[18px] leading-[26px] font-regular text-[#1D2948] whitespace-nowrap">
-                Top brands that use QR codes:
-              </p>
-              <div className="overflow-hidden w-full">
-                <div className="flex items-center gap-6 w-max animate-marquee">
-                  {/* first set */}
-                  <Image
-                    src="/images/brand/toyota.svg"
-                    alt="Toyota"
-                    width={96}
-                    height={24}
-                    className="h-5 desktop:h-6 shrink-0"
-                  />
-                  <Image
-                    src="/images/brand/netflix.svg"
-                    alt="Netflix"
-                    width={96}
-                    height={24}
-                    className="h-5 desktop:h-6 shrink-0"
-                  />
-                  <Image
-                    src="/images/brand/disney.svg"
-                    alt="Disney"
-                    width={96}
-                    height={28}
-                    className="h-5 desktop:h-7 shrink-0"
-                  />
-                  <Image
-                    src="/images/brand/oracle.svg"
-                    alt="Oracle"
-                    width={96}
-                    height={24}
-                    className="h-5 desktop:h-6 shrink-0"
-                  />
-                  <Image
-                    src="/images/brand/delta.svg"
-                    alt="Delta"
-                    width={96}
-                    height={24}
-                    className="h-5 desktop:h-6 shrink-0"
-                  />
-                  <Image
-                    src="/images/brand/fedex_express.svg"
-                    alt="FedEx Express"
-                    width={96}
-                    height={24}
-                    className="h-5 desktop:h-6 shrink-0"
-                  />
-
-                  {/* duplicate set for infinite loop */}
-                  <Image
-                    src="/images/brand/toyota.svg"
-                    alt="Toyota"
-                    width={96}
-                    height={24}
-                    className="h-5 desktop:h-6 shrink-0"
-                  />
-                  <Image
-                    src="/images/brand/netflix.svg"
-                    alt="Netflix"
-                    width={96}
-                    height={24}
-                    className="h-5 desktop:h-6 shrink-0"
-                  />
-                  <Image
-                    src="/images/brand/disney.svg"
-                    alt="Disney"
-                    width={96}
-                    height={28}
-                    className="h-5 desktop:h-7 shrink-0"
-                  />
-                  <Image
-                    src="/images/brand/oracle.svg"
-                    alt="Oracle"
-                    width={96}
-                    height={24}
-                    className="h-5 desktop:h-6 shrink-0"
-                  />
-                  <Image
-                    src="/images/brand/delta.svg"
-                    alt="Delta"
-                    width={96}
-                    height={24}
-                    className="h-5 desktop:h-6 shrink-0"
-                  />
-                  <Image
-                    src="/images/brand/fedex_express.svg"
-                    alt="FedEx Express"
-                    width={96}
-                    height={24}
-                    className="h-5 desktop:h-6 shrink-0"
-                  />
-                </div>
               </div>
             </div>
           </div>
