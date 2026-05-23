4687308fffdc8d6957c2c906fb9abc3d4f921a21
Md Arafat Akhter <arafatakhterforhad@gmail.com>
2026-05-23 11:42:22 +0600

    business image upload

--- store/slices/businessSlice.tsx
+++ store/slices/businessSlice.tsx
@@ -9,8 +9,9 @@ import {
   TimeFormat,
 } from "@/types/business";
 import { ColorPalette } from "@/types/menu";
-import { SocialChannel } from "@/types/vCard";
+import { ProfileImage, SocialChannel } from "@/types/vCard";
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { UploadLogoResponse } from "./qrSlice";
 
 function createButtonId() {
   return `button-${Date.now()}-${Math.random().toString(36).slice(2)}`;
@@ -46,6 +47,13 @@ const palette = [
     secondary: "#FFFFFF",
   },
 ];
+
+const emptyUploadedImage = {
+  publicId: "",
+  resourceType: "",
+  format: "",
+  bytes: 0,
+};
 
 const initialState: BusinessSlice = {
   colorPalette: palette,
@@ -89,6 +97,7 @@ const initialState: BusinessSlice = {
   welcomeScreen: "",
   qrCodeName: "",
   isPreviewWelcomeScreen: false,
+  uploadedBusinessImage: emptyUploadedImage,
 };
 
 const menuSlice = createSlice({
@@ -313,7 +322,9 @@ const menuSlice = createSlice({
         times: OpeningTime["times"];
       }>,
     ) => {
-      const entry = state.openingHours.find((h) => h.day === action.payload.day);
+      const entry = state.openingHours.find(
+        (h) => h.day === action.payload.day,
+      );
       if (entry) {
         entry.times = action.payload.times;
       }
@@ -332,6 +343,12 @@ const menuSlice = createSlice({
     },
     setActiveColorIndex: (state, action: PayloadAction<number>) => {
       state.activeColorIndex = action.payload;
+    },
+    setUploadedBusinessImage: (
+      state,
+      action: PayloadAction<ProfileImage | UploadLogoResponse>,
+    ) => {
+      state.uploadedBusinessImage = action.payload;
     },
   },
 });
@@ -376,5 +393,6 @@ export const {
   setQrCodeName,
   setIsPreviewWelcomeScreen,
   setActiveColorIndex,
+  setUploadedBusinessImage,
 } = menuSlice.actions;
 export default menuSlice.reducer;
