c676d83ad2f0fb4ca6a81cf2e78713485a0a1483
monaim3 <monaimmukul75@gmail.com>
2026-07-03 19:46:14 +0600

    [Generator] Page jumps when we close the logo modal  fixed

--- components/generator/Mobile_Preview_Modal.tsx
+++ components/generator/Mobile_Preview_Modal.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import PreviewQRButtons from "./Preview_QR_Buttons";
 import MobileFrame from "../common/MobileFrame";
 import CloseCircle from "../icons/CloseCircle";
@@ -20,6 +20,21 @@ export default function MobilePreviewModal({
   activeTab,
   onTabChange,
 }: MobilePreviewModalProps) {
+  useEffect(() => {
+    if (isOpen) {
+      const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
+      document.body.style.paddingRight = `${scrollbarWidth}px`;
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.paddingRight = "";
+      document.body.style.overflow = "";
+    }
+    return () => {
+      document.body.style.paddingRight = "";
+      document.body.style.overflow = "";
+    };
+  }, [isOpen]);
+
   if (!isOpen) return null;
 
   return (
